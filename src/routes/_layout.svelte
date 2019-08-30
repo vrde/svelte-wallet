da94027ae1e15d16cc1f48ab9fd7cd3f4a620809
vrde <agranzot@gmail.com>
2019-08-30 22:44:40 +0200

    Remove scaffold UI

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -7,16 +7,11 @@
 <style>
 	main {
 		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
 	}
 </style>
-
-<Nav {segment}/>
 
 <main>
 	<slot></slot>
 </main>
+
+<Nav {segment}/>
---
+++ src/routes/settings.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>Settings</title>
+</svelte:head>
+
+<h1>Settings</h1>
+
+<ul>
+  <li>
+    <a href="about">About</a>
+  </li>
+</ul>
